d516e184a7013025c2f12fc0b422f72d4e3f7f33
Malekai-git <115740812+Malekai-git@users.noreply.github.com>
2023-09-08 11:29:57 -0400

    Update app.py

--- app.py
+++ app.py
@@ -16,22 +16,27 @@ def scrape_salary():
     soup = BeautifulSoup(html, 'html.parser')
     text = soup.get_text()
     
-    pattern = r"(\$\s*\d{1,3}(?:[,\s]\d{3})*)\s*-\s*(\$\s*\d{1,3}(?:[,\s]\d{3})*)?\s*(\/\s*\w+)?"
-    matches = re.findall(pattern, text)
-    salary_data = []
-
-    for match in matches:
-        min_salary = match[0].replace('$', '').replace(',', '').strip() if match[0] else 'Unknown'
-        max_salary = match[1].replace('$', '').replace(',', '').strip() if match[1] else 'Unknown'
-        period = match[2].replace('/', '').strip() if len(match) > 2 and match[2] else 'Unknown'
-
-        salary_data.append({
-            "min_salary": min_salary,
-            "max_salary": max_salary,
-            "period": period,
-        })
+    # Updated regex pattern to capture currency and time period
+    pattern = r"(\W|^)(?P<currency>\$)?\s*(?P<min>\d{1,3}(?:[,\s]\d{3})*)(?:\s*-\s*(?P<max>\d{1,3}(?:[,\s]\d{3})*))?\s*(?P<period>\/\s*\w+)?"
     
-    return jsonify({"salaries": salary_data})
+    salaries = re.findall(pattern, text)
+    
+    # Creating a list to hold the extracted data in dictionary form
+    extracted_salaries = []
+    for salary_match in salaries:
+        currency = salary_match[1] if salary_match[1] else 'Unknown'  # Adjusted to correct index
+        min_salary = salary_match[2].replace(',', '').replace(' ', '').strip() if salary_match[2] else 'Unknown'  # Cleaned up
+        max_salary = salary_match[3].replace(',', '').replace(' ', '').strip() if salary_match[3] else 'Unknown'  # Cleaned up
+        period = salary_match[4].replace('/', '').strip() if salary_match[4] else 'Unknown'
+        
+        extracted_salaries.append({
+            'currency': currency,
+            'min_salary': min_salary,
+            'max_salary': max_salary,
+            'period': period
+        })
+
+    return jsonify({"salaries": extracted_salaries})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
