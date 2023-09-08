f705e9b060dd4b063bae54eb753a3b321cd00ad4
Malekai-git <115740812+Malekai-git@users.noreply.github.com>
2023-09-08 11:40:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def scrape_salary():
     soup = BeautifulSoup(html, 'html.parser')
     text = soup.get_text()
     
-    # Updated regex pattern to capture currency and time period
+    # Updated regex pattern
     pattern = r"(\W|^)(?P<currency>\$)?\s*(?P<min>\d{1,3}(?:[,\s]\d{3})*)(?:\s*-\s*(?P<max>\d{1,3}(?:[,\s]\d{3})*))?\s*(?P<period>\/\s*\w+)?"
     
     salaries = re.findall(pattern, text)
@@ -24,19 +24,17 @@ def scrape_salary():
     # Creating a list to hold the extracted data in dictionary form
     extracted_salaries = []
     for salary_match in salaries:
-        currency = salary_match[1] if salary_match[1] else 'Unknown'  # Adjusted to correct index
-        min_salary = salary_match[2].replace(',', '').replace(' ', '').strip() if salary_match[2] else 'Unknown'  # Cleaned up
-        max_salary = salary_match[3].replace(',', '').replace(' ', '').strip() if salary_match[3] else 'Unknown'  # Cleaned up
-        period = salary_match[4].replace('/', '').strip() if salary_match[4] else 'Unknown'
+        currency = salary_match[2] if salary_match[2] else 'Unknown'
+        min_salary = salary_match[3] if salary_match[3] else 'Unknown'
+        max_salary = salary_match[4] if salary_match[4] else 'Unknown'
         
         extracted_salaries.append({
             'currency': currency,
             'min_salary': min_salary,
-            'max_salary': max_salary,
-            'period': period
+            'max_salary': max_salary
         })
 
     return jsonify({"salaries": extracted_salaries})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(port=5000)
